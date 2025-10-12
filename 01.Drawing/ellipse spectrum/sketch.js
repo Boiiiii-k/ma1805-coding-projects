231ee5a083a50b88708b67a0de1b2c0fd8b1ff72
kevin <kevintheminion217@gmail.com>
2025-10-12 17:01:15 +0100

    ellipse spectrum

---
+++ 01.Drawing/ellipse spectrum/sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  createCanvas(600, 600);
+  noStroke();
+  background(245);
+
+}
+
+function draw() {
+
+  fill(255, 0, 0);
+  ellipse(100, 300, 100, 100);
+
+  fill(255, 128, 0);
+  ellipse(200, 300, 100, 100);
+
+  fill(255, 255, 51);
+  ellipse(300, 300, 100, 100);
+
+  fill(153, 255, 51);
+  ellipse(400, 300, 100, 100);
+
+  fill(51, 255, 51);
+  ellipse(500, 300, 100, 100);
+}
